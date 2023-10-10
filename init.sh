615f08e58ef6194ddfd3c2d6f37bd43dc64c793c
Harper Reed <harper@nata2.org>
2023-10-10 10:43:00 -0500

    updated init

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 git submodule update --init --recursive
 cd bigpy
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
 cd ../big-renderer
-npm install
+pip install -r requirements.txt
 cd ../big-pptx
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
